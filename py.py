d29bf169838415473102978aae21b17523ee989b
Egesha675 <tetenkinevgenij@gmail.com>
2025-02-10 21:44:06 +0800

    fdugf

---
+++ py.py
@@ -0,0 +1,23 @@
+# c=0
+# a = []
+# while True:
+#     line = input()
+#     if line == '0':
+#         break
+#     a.append(int(line))
+
+
+# for u in a:
+#     if u%2==0 and u%9==0:
+#         c+=1
+# print(c)
+# n = float(input())
+# binary = ''
+# while n > 0:
+#     n *= 2
+#     if n >= 1:
+#         binary += '1'
+#         n -= 1
+#     else:
+#         binary += '0'
+# print('0.' + binary if binary else '0.0')
